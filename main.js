f4ed5699ca62f86ff8116666eea6432bccc59319
bogachenkopetr <56554801+bogachenkopetr@users.noreply.github.com>
2020-03-19 22:28:50 +0300

    внес исправления в js

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
          $('.menu_topnav').hide();
         $('.burger_close').hide();
      });
+    
+    /*reviews slider*/
     const slides = $('.clients_reviews li');
     const radioButtons = $('.clients_reviews_switch button');
     radioButtons.map(function(index, button){
@@ -17,6 +19,35 @@ $(document).ready(function(){
             slides.hide();
             $(slides[index]).show();
         })
-    })
+    });
     
+    /*about_us carousel*/
+    const carousel = $('.about_us_photo');
+    const carouselItems = $('.about_us_photo li');
+    const buttonNext = $('.button_right');
+    const buttonBack = $('.button_left');
+    const displayCount = 1; // колличество изображений на которые сдвигается карусель
+    let position = 0;// это начальная позиц элемента кар-ли ul
+    
+    buttonNext.click(function(){
+        let width = $(carouselItems[0]).width() + parseFloat($(carouselItems[0]).css('margin-right')); // ширина картинки + margin right
+        console.log(width);
+        position = position - width;
+        const positionMax = - width * (carouselItems.length-displayCount); 
+        position = Math.max(position, positionMax);// позволяет в карусели прекратить смещение, когда закончились фото
+        carousel.css('margin-left', position + 'px');
+    })
+    buttonBack.click(function(){
+        let width = $(carouselItems[0]).width() + parseFloat($(carouselItems[0]).css('margin-right')); // ширина картинки + margin right
+        console.log(width);
+        position = position + width;
+        position = Math.min(position, 0);// позволяет в карусели прекратить смещение, когда закончились фото
+        carousel.css('margin-left', position + 'px');
+    })
 });
+
+
+
+
+
+
